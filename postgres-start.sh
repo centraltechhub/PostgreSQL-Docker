bbd5a83494dc531176880720aa5d1715777e7b5a
centraltechhub <centraltechhub@gmail.com>
2023-02-16 16:21:29 +0530

    Version 1

--- postgres-start.sh
+++ postgres-start.sh
@@ -4,6 +4,6 @@ echo "DB started successfullty"
 sleep 10
 createdb $DB_NAME > /home/postgres/dbcreate.log
 echo $DB_NAME + " DB created successfullty"
-psql -h localhost -p 5432 -U postgres $DB_NAME -c 'CREATE SCHEMA IF NOT EXISTS "OMS" AUTHORIZATION "postgres";'
+psql -h localhost -p 5432 -U postgres $DB_NAME -c 'CREATE SCHEMA IF NOT EXISTS "appdb" AUTHORIZATION "postgres";'
 echo "SCHEMA postgres created successfully"
 tail -f /home/postgres/dbstart.log
